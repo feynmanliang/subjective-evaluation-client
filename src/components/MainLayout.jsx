8f38a7a27abe009b69a3aed839a1b5ec8d94d79b
Feynman Liang <feynman.liang@gmail.com>
2016-06-23 11:45:29 +0100

    React router with init state in MainLayout

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,4 +1,65 @@
+import R from 'ramda';
+
 import React,{Component} from 'react';
+
+const data = {
+    "id": "testExperiment",
+    "original": [
+        {
+            "name": "Beethoven",
+            "url": "http://www.midiworld.com/classic.htm/beethoven.htm"
+        },
+        {
+            "name": "Bach",
+            "url": "http://www.midiworld.com/midis/other/c1/J_C_Bach_Ach_dass_ich_Wassers_gnug_hatte.mid"
+        }
+    ],
+    "generated": [
+        {
+            "name": "Hindemith",
+            "url": "http://www.midiworld.com/midis/other/c1/hindbsn1.mid"
+        },
+        {
+            "name": "Poulenc",
+            "url": "http://www.midiworld.com/midis/other/poulenc/poulcmo1.mid"
+        }
+    ]
+};
+
+function makeProps(experimentData) {
+  const experimentId = experimentData.id
+  const original = experimentData.original
+  const generated = experimentData.generated
+
+  let questions = []
+  for (let i = 0; i < 5; ++i) {
+    // TODO: support 1 orig vs multile genereated
+    // TODO: support multiple origs (requires multiple correctIndex)
+    // TODO: make number of questions configurable in experiment.json
+    // TODO: make num orig vs num gen configurable in experiment.json
+    const origIdx = Math.floor(Math.random() * original.length);
+    const genIdx = Math.floor(Math.random() * generated.length); // TODO: prevent duplicate questions
+
+    let choices = [ original[origIdx], generated[genIdx] ];
+    const correctIndex = Math.floor(Math.random() * choices.length);
+    choices[0] = choices[correctIndex]
+    choices[correctIndex] = original[origIdx]
+
+    const question = {
+      experimentId,
+      choices,
+      correctIndex
+    }
+    questions.push(question);
+  }
+
+  return {
+    active: {
+      question: R.head(questions)
+    },
+    questions: R.tail(questions)
+  };
+}
 
 export default class MainLayout extends Component {
   render() {
@@ -6,7 +67,7 @@ export default class MainLayout extends Component {
         <header className="primary-header"></header>
         <aside className="primary-aside"></aside>
         <main>
-          {this.props.children}
+          {React.cloneElement(this.props.children, makeProps(data))}
         </main>
       </div>;
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 import 'babel-polyfill' // required for fetch
 
-import R from 'ramda';
-
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
@@ -12,39 +11,16 @@ import reducer from './redux/reducer';
 import MainLayout from './components/MainLayout';
 import Quiz from './components/Quiz';
 
-export const store = createStore(reducer);
+const store = createStore(reducer);
+
+const routes = <Route component={MainLayout}>
+  <Route path="/" component={Quiz} />
+</Route>;
 
 // load experiment samples to generate questions from
 fetch('http://localhost:3000/')
   .then(response => response.json())
-  .then(experimentData => {
-    const experimentId = experimentData.id
-    const original = experimentData.original
-    const generated = experimentData.generated
-
-    let questions = []
-    for (let i = 0; i < 5; ++i) {
-      // TODO: support 1 orig vs multile genereated
-      // TODO: support multiple origs (requires multiple correctIndex)
-      // TODO: make number of questions configurable in experiment.json
-      // TODO: make num orig vs num gen configurable in experiment.json
-      const origIdx = Math.floor(Math.random() * original.length);
-      const genIdx = Math.floor(Math.random() * generated.length); // TODO: prevent duplicate questions
-
-      let choices = [ original[origIdx], generated[genIdx] ];
-      const correctIndex = Math.floor(Math.random() * choices.length);
-      choices[0] = choices[correctIndex]
-      choices[correctIndex] = original[origIdx]
-
-      const question = {
-        experimentId,
-        choices,
-        correctIndex
-      }
-      questions.push(question);
-    }
-    return questions;
-  })
+  .then()
   // .then(questions => {
   //   store.dispatch({
   //     type: 'SET_QUESTIONS',
@@ -53,11 +29,7 @@ fetch('http://localhost:3000/')
   // })
   .then((questions) => {
     ReactDOM.render(
-      <MainLayout>
-        <Quiz active={{
-          question: R.head(questions)
-        }} questions={R.tail(questions)} />
-      </MainLayout>,
+      <Router history={hashHistory}>{routes}</Router>,
       document.getElementById('root'));
   });
 
